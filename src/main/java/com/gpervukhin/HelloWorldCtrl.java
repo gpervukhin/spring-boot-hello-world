e093ee4e7b9b0b39b706d05a087ec4dc9acd2ba8
Gennadiy Pervukhin <gpervukhin@gmail.com>
2018-12-29 12:50:07 +0300

    test2

--- src/main/java/com/gpervukhin/HelloWorldCtrl.java
+++ src/main/java/com/gpervukhin/HelloWorldCtrl.java
@@ -3,6 +3,7 @@ package com.gpervukhin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,5 +15,15 @@ public class HelloWorldCtrl
 		ModelAndView mav = new ModelAndView("home");
 		mav.getModel().put("name", name);
 		return mav;
+	}
+	
+	@RequestMapping("/testLong")
+	@ResponseBody
+	public String testLong()
+	{
+		try { Thread.sleep(10000); }
+		catch (InterruptedException e) {}
+		
+		return "Result after delay";
 	}
 }
